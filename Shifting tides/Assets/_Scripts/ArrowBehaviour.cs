8cf2c5c4bfcd425eb8ae1ec6167ce2ccf3bf7cc8
Semih Orhan PC <s.orhan@hva.nl>
2018-02-24 22:09:35 +0100

    "Arrows" now stay on impact location and do not collide anymore.
    [Scripts]
    -ArrowBehaviour:
    On impact the arrow location and rotation get saved in two separate variables.
    These get inserted into the arrowPlaceholder on instantiating.
    The old arrow gets destroyed.
    The new arrow gets destroyed after 3 seconds.
    
    -DestroyGameObject:
    Simple script dat destroys the gameobject it is on after a specified amount of time.

---
+++ Shifting tides/Assets/_Scripts/ArrowBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowBehaviour : MonoBehaviour
+{
+    private Vector3 arrowPlaceholderPosition, arrowPlaceholderRotation;
+    private GameObject arrowPlaceholder;
+
+    // Use this for initialization
+    void Start()
+    {
+        arrowPlaceholder = Resources.Load("Prefabs/ArrowPlaceholder") as GameObject;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Ground":
+                Debug.Log("Hitting: " + other.gameObject.tag);
+                SetupArrowPlaceholder();
+                break;
+            case "Wall":
+                Debug.Log("Hitting: " + other.gameObject.tag);
+                SetupArrowPlaceholder();
+                break;
+        }
+    }
+
+    // This method gets the current postion and rotation of the arrow and hold them in
+    // two separate variables.
+    private void CopyPositionAndRotationForArrowPlaceholder()
+    {
+        arrowPlaceholderPosition = transform.position;
+        arrowPlaceholderRotation = transform.eulerAngles;
+    }
+
+    // This method calls CopyPositionAndRotationForArrowPlaceholder.
+    // Then it instantiates the ArrowPlaceholder with the values from CopyPositionAndRotationForArrowPlaceholder and destroys the current arrow.
+    private void SetupArrowPlaceholder()
+    {
+        CopyPositionAndRotationForArrowPlaceholder();
+        Instantiate(arrowPlaceholder, arrowPlaceholderPosition, arrowPlaceholder.transform.rotation = Quaternion.Euler(arrowPlaceholderRotation));
+        Destroy(gameObject);
+    }
+}
